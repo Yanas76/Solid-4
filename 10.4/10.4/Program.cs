bd8e6992e4934c68954f3ba7bc8ab98cedf876f5
Yanas76 <116164567+Yanas76@users.noreply.github.com>
2022-12-02 00:15:34 +0200

    Add files via upload

---
+++ 10.4/10.4/Program.cs
@@ -0,0 +1,58 @@
+﻿//Порушено принцип розділення інтерфейсу
+using System.Runtime.CompilerServices;
+
+interface IItem
+{  
+    void SetPrice(double price);
+}
+
+interface IDiscount
+{
+    void ApplyDiscount(String discount);
+}
+
+interface IPromocode
+{
+    void ApplyPromocode(String promocode);
+}
+
+interface IClothing
+{
+    void SetColor(byte color);
+    void SetSize(byte size);
+}
+
+class Book: IItem, IDiscount
+{
+    public void SetPrice(double price)
+    {
+
+    }
+    public void ApplyDiscount(String discount)
+    {
+
+    }
+
+}
+
+class Clothing : IItem, IClothing, IDiscount
+{
+    public void SetPrice(double price)
+    {
+
+    }
+
+    public void ApplyDiscount(String discount)
+    {
+
+    }
+
+    public void SetColor(byte color)
+    {
+
+    }
+    public void SetSize(byte size)
+    {
+
+    }
+}
